80c337952f430241af6b339111eeaab4d6942bb7
Yogendra Singh Chouhan <yogendrar@109gmail.com>
2025-08-12 21:43:35 +0530

    new version

--- src/apis/EndPoint.jsx
+++ src/apis/EndPoint.jsx
@@ -1,5 +1,4 @@
-//export const BASE_URL = "https://health-suggestion-app-backend.onrender.com"
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://health-suggestion-app-backend.onrender.com"
 export default {
   SIGN_UP: BASE_URL+"/user",
   SIGN_IN: BASE_URL+"/user/authentication",
